028b1dc696ac188e33099f691ae072dc3e04beae
EnverGeekBrains <41239252+EnverGeekBrains@users.noreply.github.com>
2018-09-16 15:53:41 +0300

    HomeWork

---
+++ WorkDirect/Course.java
@@ -0,0 +1,25 @@
+package ru.eskendarov.ea.HomeWork_1;
+
+class Course {
+    private int[] obs;
+
+    Course(int[] obs) {
+        this.obs = obs;
+    }
+
+    void doIt(Team st) {
+        for (int i = 0; i < st.opp.length; i++) {
+            int max = 0;
+            for (int j : obs) {
+                if (j > max) {
+                    max = j;
+                }  // вычислим макс. препятствие
+            }
+            if (st.opp[i] > max) {
+                st.idWinner[i] = 1;
+            } // если возможность превышает препятствие, записываем победителя
+
+        }
+    }
+}
+
